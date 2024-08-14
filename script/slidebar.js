0fda2b6dc83ef18ca69367ee239e173e4efac192
vijaygovindBiju <vijaygovindbiju@gmail.com>
2024-08-14 16:28:06 +0530

    Add files via upload

---
+++ script/slidebar.js
@@ -0,0 +1,136 @@
+function showSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'flex'
+}
+function hideSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none'
+}
+
+
+
+
+const the_animation1 = document.querySelectorAll('.glass1')
+
+const observer1 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('scroll-animation')
+        }
+            else {
+                entry.target.classList.remove('scroll-animation')
+            }
+        
+    })
+},
+   { threshold: 0.5
+   });
+//
+  for (let i = 0; i < the_animation1.length; i++) {
+   const elements = the_animation1[i];
+
+    observer1.observe(elements);
+  };
+
+
+
+
+  const the_animation2 = document.querySelectorAll('.glass2')
+
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('scroll-animation')
+        }
+            else {
+                entry.target.classList.remove('scroll-animation')
+            }
+        
+    })
+},
+   { threshold: 0.5
+   });
+//
+  for (let i = 0; i < the_animation2.length; i++) {
+   const elements = the_animation2[i];
+
+    observer2.observe(elements);
+  };
+
+
+
+
+
+
+  const the_animation3 = document.querySelectorAll('.glass3')
+
+const observer3 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('scroll-animation')
+        }
+            else {
+                entry.target.classList.remove('scroll-animation')
+            }
+        
+    })
+},
+   { threshold: 0.5
+   });
+//
+  for (let i = 0; i < the_animation3.length; i++) {
+   const elements = the_animation3[i];
+
+    observer3.observe(elements);
+  };
+
+
+
+  
+  const the_animation4 = document.querySelectorAll('.glass4')
+
+const observer4 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('scroll-animation')
+        }
+            else {
+                entry.target.classList.remove('scroll-animation')
+            }
+        
+    })
+},
+   { threshold: 0
+   });
+//
+  for (let i = 0; i < the_animation4.length; i++) {
+   const elements = the_animation4[i];
+
+    observer4.observe(elements);
+  };
+
+
+
+
+  const the_animation = document.querySelectorAll('.glass')
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('scroll-animation')
+        }
+            else {
+                entry.target.classList.remove('scroll-animation')
+            }
+        
+    })
+},
+   { threshold: 0.5
+   });
+//
+  for (let i = 0; i < the_animation.length; i++) {
+   const elements = the_animation[i];
+
+    observer.observe(elements);
+  };
+
